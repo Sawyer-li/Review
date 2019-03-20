e914df0886b438638fb03b5c82c1fe11b9df8efc
sawyerli <sawyerl@oschain.io>
2019-03-20 12:24:28 +0800

    基本静态布局

--- src/containers/Home/components/Likelist/index.js
+++ src/containers/Home/components/Likelist/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import LikeItem from '../LikeItem'
+import Loading from '../../../../components/Loading'
 import './style.css'
 const dataSource = [
   {
@@ -65,19 +66,64 @@ const dataSource = [
 ];
 
 export default class Likelist extends Component {
+  constructor(props){
+    super(props)
+    this.myRef = React.createRef();
+    this.state = {
+      data: dataSource,
+      loadTimes: 1
+    }
+    this.removeListener = false;
+  }
   render() {
-    const data = dataSource;
+    const {data, loadTimes}= this.state;
     return (
-      <div className="likeList">
+      <div ref={this.myRef} className="likeList">
         <div className="likeList__header">猜你喜欢</div>
         <div className="likeList__list">
           {
             data.map((item, index)=>{
-              return <LikeItem key={item.id}  data={item}/>
+              return <LikeItem key={item.index}  data={item}/>
             })
           } 
         </div>
+        {
+          loadTimes < 3 ? (<Loading />) : (
+            <a href="" className="likeList__viewAll">加载更多</a>
+          )
+        }
       </div>
     )
+  }
+  componentDidMount() {
+    document.addEventListener("scroll", this.handleScroll);
+  }
+  componentDidUpdate() {
+    if (this.state.loadTimes >= 3 && !this.removeListener){
+      document.removeEventListener("scroll", this.handleScroll);
+      this.removeListener = true;
+    }
+  }
+  componentWillUnmount(){
+    it(!this.removeListener)
+      document.removeEventListener("scroll", this.handleScroll);
+  }
+  //处理滚动事件实现加载更多
+  handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const screenHeight = document.documentElement.clientHeight;
+    const likeListTop = this.myRef.current.offsetTop;
+    const likeListHeight = this.myRef.current.offsetHeight;
+    //当下拉到底部时
+    if(scrollTop+45>= likeListHeight + likeListTop - screenHeight){
+        const newData = this.state.data.concat(dataSource);
+        const newLoadTimes = this.state.loadTimes + 1;
+        setTimeout(()=> {
+          this.setState({
+            data: newData,
+            loadTimes: newLoadTimes
+          })
+        },1000) 
+    }
   }
 }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,15 +3,21 @@ import Category from "./components/Category";
 import Headline from "./components/Headline";
 import Discount from "./components/Discount";
 import Likelist from "./components/Likelist";
+import HomeHeader from './components/HomeHeader'
+import Banner from './components/Banner'
+import Footer from '../../components/Footer'
 import './style.css';
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <HomeHeader/>
+        <Banner/>
         <Category />
         <Headline / >
         <Discount />
         <Likelist />
+        <Footer />
       </div>
     );
   }
