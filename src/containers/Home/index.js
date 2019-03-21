9e95a44548d84f186e95b457e64599053c4d13d4
sawyerli <sawyerl@oschain.io>
2019-03-22 00:34:35 +0800

    react 和 redux链接

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,24 +1,62 @@
 import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 import Category from "./components/Category";
 import Headline from "./components/Headline";
 import Discount from "./components/Discount";
 import Likelist from "./components/Likelist";
-import HomeHeader from './components/HomeHeader'
-import Banner from './components/Banner'
-import Footer from '../../components/Footer'
-import './style.css';
-export default class Home extends Component {
+import HomeHeader from "./components/HomeHeader";
+import Banner from "./components/Banner";
+import Footer from "../../components/Footer";
+import {
+  actions as homeActions,
+  getLikes,
+  getDiscounts,
+  getPageCountOfLikes
+} from "../../redux/modules/home";
+const mapStateToProps = (state, props) => {
+  return {
+    likes: getLikes(state),
+    discounts: getDiscounts(state),
+    pageCount: getPageCountOfLikes(state)
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    homeActions: bindActionCreators(homeActions, dispatch)
+  };
+};
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.homeActions.loadDiscounts();
+  }
+
+  fetchMoreLikes = () => {
+    this.props.homeActions.loadLikes();
+  };
   render() {
+    console.log(this.props);
+    const { likes, discounts, pageCount } = this.props;
     return (
       <div>
-        <HomeHeader/>
-        <Banner/>
+        <HomeHeader />
+        <Banner />
         <Category />
-        <Headline / >
-        <Discount />
-        <Likelist />
+        <Headline />
+        <Discount data={discounts} />
+        <Likelist
+          data={likes}
+          pageCount={pageCount}
+          fetchData={this.fetchMoreLikes}
+        />
         <Footer />
       </div>
     );
   }
 }
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home);
